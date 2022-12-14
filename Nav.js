227ffcbd2582151650c47ffc49377b8c3628d0dd
Gagandeep Singh <57759605+50R1Paps@users.noreply.github.com>
2022-12-14 14:43:04 +0100

    Modified the Navbar effect

--- Nav.js
+++ Nav.js
@@ -1,17 +1,25 @@
 $(document).ready(function() {
-    // cool nav menu
-    $(window).on('load resize', function() {
-      var $thisnav = $('.current-menu-item').offset().left;
-  
-      $('.menu-item').hover(function() {
-        var $left = $(this).offset().left - $thisnav;
-        var $width = $(this).outerWidth();
-        var $start = 0;
-        $('.wee').css({ 'left': $left , 'width': $width });
-      }, function() {
-        var $initwidth = $('.current-menu-item').width();
-        $('.wee').css({ 'left': '0' , 'width': $initwidth });
-      });
+  // cool nav menu
+  $(window).on('load resize', function() {
+    var $thisnav = $('.current-menu-item').offset().left;
+    var $left = $('.current-menu-item').offset().left - $thisnav;
+    var $width = $('.current-menu-item').outerWidth();
+
+    $('.menu-item').hover(function() {
+      var $left = $(this).offset().left - $thisnav;
+      var $width = $(this).outerWidth();
+      $('.wee').css({ 'left': $left , 'width': $width });
+    }, function() {
+      $('.wee').css({ 'left': $left , 'width': $width });
     });
-  
+
+    // modify code to maintain left and width when element is clicked
+    $('.menu-item').click(function() {
+      $left = $(this).offset().left - $thisnav;
+      $width = $(this).outerWidth();
+      $('.wee').css({ 'left': $left , 'width': $width });
+      $('.current-menu-item').removeClass('current-menu-item');
+      $(this).addClass('current-menu-item');
+    });
   });
+});
