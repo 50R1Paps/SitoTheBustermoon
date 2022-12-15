f0f9749c7212a1aae04438f5904df6b4b0d11c73
Gagandeep Singh <57759605+50R1Paps@users.noreply.github.com>
2022-12-15 15:44:07 +0100

    Scroll function does not activate when click function activates

--- Nav.js
+++ Nav.js
@@ -1,32 +1,33 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // cool nav menu
-  $(window).on('load resize', function() {
-    var $thisnav = $('.current-menu-item').offset().left;
-    var $left = $('.current-menu-item').offset().left - $thisnav;
-    var $width = $('.current-menu-item').outerWidth();
+  $(window).on("load resize", function () {
+    var $thisnav = $(".current-menu-item").offset().left;
+    var $left = $(".current-menu-item").offset().left - $thisnav;
+    var $width = $(".current-menu-item").outerWidth();
+
     // Store the original left and width of the element
-    var originalLeft = $('.current-menu-item').offset().left;
-    var originalWidth = $('.current-menu-item').outerWidth();
+    var originalLeft = $(".current-menu-item").offset().left;
+    var originalWidth = $(".current-menu-item").outerWidth();
 
     var clickEventExecuted = false;
-   
+
     // modify code to maintain left and width when element is clicked
-    $('.menu-item').click(function() {
+    $(".menu-item").click(function () {
       clickEventExecuted = true;
       $left = $(this).offset().left - $thisnav;
       $width = $(this).outerWidth();
-      $('.wee').css({ 'left': $left , 'width': $width });
-      $('.current-menu-item').removeClass('current-menu-item');
-      $(this).addClass('current-menu-item');
+      $(".wee").css({ left: $left, width: $width });
+      $(".current-menu-item").removeClass("current-menu-item");
+      $(this).addClass("current-menu-item");
       var snd = new Audio("sound1.wav"); // buffers automatically when created
       snd.play();
-      //clickEventExecuted = false
+      setTimeout(function () {
+        clickEventExecuted = false;
+      }, 1200);
     });
 
-  
-/*
-// Add a function that is executed on the 'scroll' event
-    $(window).on('scroll', function() {
+    // Add a function that is executed on the 'scroll' event
+    $(window).on("scroll", function () {
       if (clickEventExecuted) {
         return;
       }
@@ -36,33 +37,32 @@ $(document).ready(function() {
       // If the user has scrolled down
       if (scrollPos > 0) {
         // Get the current left and width of the element
-        var $left = $('.current-menu-item').offset().left;
-        var $width = $('.current-menu-item').outerWidth();
+        var $left = $(".current-menu-item").offset().left;
+        var $width = $(".current-menu-item").outerWidth();
 
         // Use the .css() method to set the left and width of the element
-        $('.wee').css({ 'left': $left, 'width': $width });
+        $(".wee").css({ left: $left, width: $width });
       }
       // If the user has scrolled up
       else {
         // Use the original left and width of the element
-        $('.wee').css({ 'left': originalLeft, 'width': originalWidth });
+        $(".wee").css({ left: originalLeft, width: originalWidth });
       }
     });
-*/
 
-
-    $('.menu-item').hover(function() {
-      var $left = $(this).offset().left - $thisnav;
-      var $width = $(this).outerWidth();
-      $('.wee').css({ 'left': $left , 'width': $width });
-    }, function() {
-      $('.wee').css({ 'left': $left , 'width': $width });
-      /*
+    $(".menu-item").hover(
+      function () {
+        var $left = $(this).offset().left - $thisnav;
+        var $width = $(this).outerWidth();
+        $(".wee").css({ left: $left, width: $width });
+      },
+      function () {
+        $(".wee").css({ left: $left, width: $width });
+        /*
       var snd = new Audio("sound3.wav");
       snd.play();
       */
-    });
-
-    
+      }
+    );
   });
 });
