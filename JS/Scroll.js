3ea121ee4cbe8ec9d978261644486aebca5ac431
Gabriele <gabriele.caletti@outlook.it>
2023-02-23 12:02:10 +0100

    New style

--- JS/Scroll.js
+++
@@ -1,6 +0,0 @@
-function generateReport(clickedElement) {
-  var targetId = clickedElement.getAttribute('href').substring(1);
-  $('html, body').animate({
-    scrollTop: $('#' + targetId).offset().top-200
-  }, 1000);
-};
